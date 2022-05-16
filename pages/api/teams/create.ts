c1bc3933805f798c5af5b5475ce37be8990a85c9
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2022-05-16 01:41:26 -0500

    feat: added the endpoint for creating a team and adding the team to users app_metadata in auth0

--- pages/api/teams/create.ts
+++ pages/api/teams/create.ts
@@ -3,10 +3,20 @@ import { createApiHelper } from '@/helpers/api-helper'
 import { auth0 } from '@/helpers/auth0-client'
 import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0'
 import { PrismaClient } from '@prisma/client';
-type Team = {
-    name: string
-    email: string
+type CreateNewTeamCommand = {
+    teamName: string
+    slug: string
     auth0Id?: string
+}
+
+type TeamCreatedResponse = {
+    team: {
+        id: number
+        name: string
+        slug: string
+        createdAt: Date | null
+        updatedAt: Date | null
+    }
 }
 
 const prisma: PrismaClient = new PrismaClient();
@@ -17,9 +27,9 @@ export default withApiAuthRequired(function handler(
     const api = createApiHelper(req, res);
     const session = getSession(req, res);
     api.post(async (request, response) => {
-        const teams: Team[] = [];
+        const teams: string[] = [];
         try {
-            const team: Team = request.body;
+            const createNewCommand: CreateNewTeamCommand = request.body;
             const user = await auth0.getUser({ id: session?.user?.sub });
             const found = await prisma.accounts.findUnique({
                 where: {
@@ -28,14 +38,78 @@ export default withApiAuthRequired(function handler(
             });
             const hasAccount = found !== null ? true : false;
             if (hasAccount) {
-                await auth0.updateAppMetadata({
-                    id: user?.user_id!
-                }, {
-                    teams
-                })
-            }
+                //TODO: Create slug const teamSlug = team.teamName.toLowerCase().replace(/\s/g, '-');
+                const model = {
+                    name: createNewCommand.teamName,
+                    slug: createNewCommand.slug
+                };
+                // Check if the team already exists
+                const existingTeam = await prisma.teams.findFirst({
+                    where: {
+                        slug: createNewCommand.slug
+                    }
+                });
+                if (existingTeam) {
+                    return response.status(400).json({
+                        message: 'Team already exists'
+                    });
+                }
+                // Get current user from auth0 and check app_metatdata for stripeCustomerId
+                const currentUser = await auth0.getUser({ id: session?.user?.sub });
+                if (currentUser?.app_metadata?.stripeCustomerId) {
+                    const subscription = await prisma.subscriptions.findFirst({
+                        where: {
+                            stripeCustomerId: currentUser?.app_metadata?.stripeCustomerId
+                        }
+                    })
+                    if (!subscription) {
+                        return response.status(400).json({
+                            error: 'You must have a subscription to create a team.'
+                        });
+                    }
+                    const teamCreated = await prisma.teams.create({
+                        data: {
+                            ...model,
+                            subscription: {
+                                connect: {
+                                    id: subscription?.id
+                                }
+                            }
+                        }
+                    });
+                    // check if account has any other teams
+                    const allTeams = await prisma.teams.findMany({
+                        where: {
+                            subscriptionId: subscription?.id
+                        }
+                    });
+                    if (allTeams && allTeams.length > 0) {
+                        allTeams.forEach(async (team) => {
+                            teams.push(team.slug);
+                        });
+                    } else {
+                        teams.push(createNewCommand.slug);
+                    }
+                    // add team to account
+                    await auth0.updateAppMetadata({
+                        id: user?.user_id!
+                    }, {
+                        teams
+                    });
+                    const teamCreatedResponse: TeamCreatedResponse = {
+                        team: {
+                            ...teamCreated
+                        }
+                    };
+                    response.status(200).json(teamCreatedResponse);
+                } else {
+                    throw new Error('Please contact your team administrator to create a team.');
+                }
 
-            response.status(200).json({});
+            } else {
+                console.log("No account was found for this user.");
+                throw new Error('Please contact your team administrator to create a team.');
+            }
         } catch (error: any) {
             await auth0.updateAppMetadata({
                 id: session?.user?.sub
