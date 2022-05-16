9c761b38b9fe01eb006f4b053b95cb78676e643a
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2022-05-15 20:28:54 -0500

    feat: scaffolded endpoint getting a stripe billing link

---
+++ pages/api/accounts/billing.ts
@@ -0,0 +1,51 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { createApiHelper } from '@/helpers/api-helper'
+import { auth0 } from '@/helpers/auth0-client'
+import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0'
+import { PrismaClient } from '@prisma/client';
+type Account = {
+    firstName: string
+    lastName: string
+    isAdmin: string
+    email: string
+    auth0Id?: string
+}
+
+const prisma: PrismaClient = new PrismaClient();
+export default withApiAuthRequired(function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    const api = createApiHelper(req, res);
+    const session = getSession(req, res);
+    api.post(async (request, response) => {
+        try {
+            // const account: Account = request.body;
+            const user = await auth0.getUser({ id: session?.user?.sub });
+            const found = await prisma.accounts.findUnique({
+                where: {
+                    auth0Id: user?.user_id
+                }
+            });
+            const hasAccount = found !== null ? true : false;
+            if (hasAccount) {
+                await auth0.updateAppMetadata({
+                    id: user?.user_id!
+                }, {
+
+                })
+            }
+
+            response.status(200).json({});
+        } catch (error: any) {
+            await auth0.updateAppMetadata({
+                id: session?.user?.sub
+            }, {
+                accountCreated: false
+            })
+            response.status(500).json({ error: error.message })
+        } finally {
+            prisma.$disconnect();
+        }
+    });
+})
