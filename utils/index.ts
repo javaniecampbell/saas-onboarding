1fe9179d52893b94ab1e47f3576c121fd26b69c2
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2022-05-15 19:38:10 -0500

    feat: created utils for classes programmatically

--- utils/index.ts
+++ utils/index.ts
@@ -2,3 +2,7 @@ export function capitalize(str: string) {
     const lower = str.toLowerCase();
     return str.charAt(0).toUpperCase() + lower.slice(1);
 }
+
+export function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ')
+}
