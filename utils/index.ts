5d35b204f540f6497aa199ba8659eff776652735
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2022-07-02 20:13:19 -0500

    feat: added utility method to exclude property from object

--- utils/index.ts
+++ utils/index.ts
@@ -6,3 +6,10 @@ export function capitalize(str: string) {
 export function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(' ')
 }
+
+export function exclude<T, Key extends keyof T>(resultSet: T, ...keys: Key[]): Omit<T, Key> {
+    for (let key of keys) {
+      delete resultSet[key];
+    }
+    return resultSet;
+  }
